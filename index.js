03302768b44e3d006e0cd054ff91323315593e86
dowpm <mcdowpm@gmail.com>
2019-02-01 23:29:51 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input');
+let ul = document.querySelector('ul.employee-list');
+
+function retrieveEmployeeInformation(){
+  return input.value;
+}
+
+function addNewElementAsLi(){
+  
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick(){
+  let addEmployee = document.querySelector("input[type=submit]");
+  addEmployee.addEventListener('click',function(e){
+    addNewElementAsLi();
+    input.value = '';
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  let clear = document.querySelector('a');
+  clear.addEventListener('click',function(e){
+    ul.innerHTML = '';
+  })
+}
